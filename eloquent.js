7aae845d579bc84a2680804d8259af2ddba3f823
Mel Booth <mel.m.booth@gmail.com>
2016-10-02 10:38:38 +1300

    update minimum with math.min method

--- eloquent.js
+++ eloquent.js
@@ -1,6 +1,14 @@
-//Minimum
+//MINIMUM
 
 //Write a function (min) that takes two arguments, and returns their minimum.
+
+function min(a, b){
+  return Math.min(a,b);
+}
+
+console.log (min (32, 7));
+
+//OR THE LONG WAY
 
 function min(a, b){
   if (a < b)
@@ -13,13 +21,26 @@ console.log(min(0, 10));
 console.log(min(0, -10));
 // → -10
 
-//Recursion
+
+
+//RECURSION
 
 //Zero is even.
 //One is odd.
 //For any other number N, its evenness is the same as N - 2.
 //Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
 //Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+//Example of recursive function (function that calls itself):
+function power(base, exponent) {
+  if (exponent == 0)
+    return 1;
+  else
+    return base * power(base, exponent - 1);
+}
+
+console.log(power(2, 3));
+// → 8
 
 
 
@@ -41,12 +62,28 @@ console.log(isEven(75));
 console.log(isEven(-1));
 // → ??
 
-function power(base, exponent) {
-  if (exponent == 0)
-    return 1;
-  else
-    return base * power(base, exponent - 1);
+
+
+//BEAN COUNTING
+
+//You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length with "s".length. The returned value will be a string containing only one character (for example, "b"). The first character has position zero, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+//Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+
+//Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+function countBs (string) {
+
+
 }
 
-console.log(power(2, 3));
-// → 8
+for (var = i; i < string.length; i++)
+
+
+
+// Your code here.
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
